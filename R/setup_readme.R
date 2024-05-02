fa86a077c638fe04813a78ba7d674e86ffe14b71
Mian <mzhong@ethz.ch>
2024-05-02 09:38:33 +0200

    update setup readme code

--- R/setup_readme.R
+++ R/setup_readme.R
@@ -1,19 +1,23 @@
-setup_readme <- function(has_example = TRUE){
-  # Create README RMarkdown with a template
-  usethis::use_readme_rmd(open = FALSE)
-  readmermd_path <- fs::path(getwd(), "README", ext = "Rmd")
+setup_readme <- function(name = "README", has_example = FALSE){
+  readmermd_path <- fs::path(name, ext = "Rmd")
   pkgname <- desc::desc_get("Package")
   dataname <- list.files("data")[1]
+  if (is.na(dataname)) {
+    warning("No tidy data found. Please revise the section DATA in README!")
+  }
+  # Create README RMarkdown with a template
+  usethis::use_readme_rmd(open = FALSE)
   usethis::use_template(template = "README.rmd",
-                        save_as = readme_path,
+                        save_as = readmermd_path,
                         data = list(packagename = pkgname,
                                     dataname = dataname),
-                        open = rlang::is_interactive(),
+                        open = FALSE,
                         package = "washr")
+
+  # devtools::build_readme()
   # Create example vignettes
   if (has_example) {
     usethis::use_article("examples")
     devtools::build_rmd("vignettes/articles/examples.Rmd")
   }
-
 }
