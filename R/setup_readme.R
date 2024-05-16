e120f19777183aac16fcae2f2f2a8e2b45b7ce6d
Mian <mzhong@ethz.ch>
2024-05-16 12:37:26 +0200

    update and test drive setup_readme

--- R/setup_readme.R
+++ R/setup_readme.R
@@ -1,32 +1,35 @@
-#' Title
+#' Generate the README rmarkdown file
 #'
-#' @param has_example
 #'
 #' @return
 #' @export
 #'
 #' @examples
-setup_readme <- function(has_example = FALSE){
+#' \dontrun{
+#' setup_dictionary()
+#' # Complete save the dictionary csvfile with variable descriptions
+#' setup_readme()
+#' }
+#'
+setup_readme <- function(){
   # Get metadata
   readmermd_path <- fs::path("README", ext = "Rmd")
   pkgname <- desc::desc_get("Package")
-  dataname <- list.files("data")[1]
+  dataname <- fs::path_ext_remove(list.files("data")[1])
   if (is.na(dataname)) {
     warning("No tidy data found. Please revise the section DATA in README!")
   }
   # Create README RMarkdown with a template
   ## We need to assume tidy data, dictionary files exist to run build_readme
+  ## TODO: sanity_check() on extdata?
   usethis::use_readme_rmd(open = FALSE)
   fs::file_delete(readmermd_path)
   usethis::use_template(template = "README.rmd",
                         save_as = readmermd_path,
                         data = list(packagename = pkgname,
                                     dataname = dataname),
-                        open = FALSE,
+                        open = rlang::is_interactive(),
                         package = "washr")
-  # Create example vignettes
-  if (has_example) {
-    usethis::use_article("examples")
-    devtools::build_rmd("vignettes/articles/examples.Rmd")
-  }
+  usethis::ui_todo("Finish the writing of README and run devtools::build_readme() in console.")
 }
+
