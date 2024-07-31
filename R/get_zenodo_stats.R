a6ee8b540edc47f2ce32651ce88d66248aeb6925
Mian Zhong <mzhong@ethz.ch>
2024-07-31 08:19:25 +0200

    refactor to function

--- R/get_zenodo_stats.R
+++ R/get_zenodo_stats.R
@@ -1,11 +1,23 @@
 library(httr)
 
-community_name <- "openwashdata"
-community_page <- httr::content(httr::GET(sprintf("https://zenodo.org/api/communities/%s", community_name)))
-records_url <- community_page$links$records
-records_url_content <- httr::content(httr::GET(records_url))
-records_list <- records_url_content$hits$hits
-for each record in records_list
-record_stats <- record$stats
+get_community_records <- function(name){
+  community_page <- httr::content(httr::GET(sprintf("https://zenodo.org/api/communities/%s", name)))
+  records_url <- community_page$links$records
+  records_url_content <- httr::content(httr::GET(records_url))
+  records_list <- records_url_content$hits$hits
+  # add request datetime
+  return(records_list)
+}
+
+get_record_by_doi <- function(doi){
+  id <- strsplit(doi, "zenodo.")[[1]][2]
+  record <- httr::content(httr::GET(sprintf("https://zenodo.org/api/records/%s", id)))
+  # add request datetime
+  return(record)
+}
+
+record_stats
 # unique view numbers
+record$stats$unique_views
 # unique downloads numbers
+record$stats$unique_downloads
